f7ed0c08b205e9b9aced8c997c1e6f3065997ba9
Sam Bautista <tadeolinco@gmail.com>
2018-04-16 02:38:03 +0800

    Add scripts for prod

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,43 +1,46 @@
-import 'reflect-metadata';
+import 'reflect-metadata'
 
-import * as bodyParser from 'body-parser';
-import * as express from 'express';
-import * as logger from 'morgan';
-import * as session from 'express-session';
-import * as store from 'express-mysql-session';
+import * as bodyParser from 'body-parser'
+import * as express from 'express'
+import * as logger from 'morgan'
+import * as path from 'path'
+import * as session from 'express-session'
+import * as store from 'express-mysql-session'
 
-import { createConnection } from 'typeorm';
-import router from './router';
+import { createConnection } from 'typeorm'
+import router from './router'
 
 createConnection()
   .then(async connection => {
-    console.log('Successfully connected to MySQL');
+    console.log('Successfully connected to MySQL')
     // create express app
-    const app = express();
-    const MySQLStore = store(session);
+    const app = express()
+    const MySQLStore = store(session)
     const sessionStore = new MySQLStore({
       user: 'root',
       database: 'pe2_ttn',
       password: 'admin',
-    });
+    })
     app.use(
       session({
         secret: 'random secret',
         store: sessionStore,
         resave: false,
         saveUninitialized: false,
-      }),
-    );
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
+      })
+    )
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({ extended: true }))
 
-    app.use(logger('dev'));
+    app.use(logger('dev'))
 
-    app.use('/api', router);
+    app.use('/api', router)
 
-    const port = process.env.PORT || 3001;
+    app.use('/', express.static(path.join(__dirname, 'build')))
+
+    const port = process.env.PORT || 3001
     app.listen(port, () => {
-      console.log(`Server listening to port: ${port}`);
-    });
+      console.log(`Server listening to port: ${port}`)
+    })
   })
-  .catch(error => console.log(error));
+  .catch(error => console.log(error))
